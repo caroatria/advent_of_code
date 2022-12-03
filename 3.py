e8bef7fe826b6e830526f1e2253878644e6b8d02
Carolina Atria <>
2022-12-03 11:57:04 +0100

    third day complete

--- 3.py
+++ 3.py
@@ -1,11 +1,10 @@
 #advend of code
 #december 3rd
 
+f = open("3_input_file", "r") #read input file
+content = f.readlines()
 
-
-
-character_scores = {"a":1,"A":27}
-
+character_scores = {"a":1,"A":27} #stores values for each character of the alphabet
 start_cha = "a"
 for char in range(1,26):
     curr_char = chr(ord(start_cha)+char)
@@ -20,16 +19,19 @@ for char in range(27,52):
     curr_char = chr(ord(curr_char)+char-26)
     char += 1
 
-def string_intersection(str1,str2):
+def string_intersection(str1,str2,str3="n/a"): #to get characters present in all strings
     res = ""
-    for i in str1:
-        if i in str2 and not i in res:
-            res += i
+    if str3 == "n/a":
+        for i in str1:
+            if i in str2 and not i in res:
+                res += i
+    else:
+        for i in str1:
+            if i in str2:
+                if i in str3 and i not in res:
+                    res += i
     return(res)
 
-f = open("3_input_file", "r")
-content = f.readlines()
-# print(content)
 common_character = []
 for line in content:
     mid = int(len(line)/2-0.5) #mid point to separate compartments
@@ -37,9 +39,22 @@ for line in content:
     second_comp = (line[mid:len(line)])
     common_character.append(string_intersection(first_comp,second_comp))
 
-    
 sum_of_chars = 0
 for char in common_character:
     sum_of_chars += character_scores[char]
-print(sum_of_chars)
+print("Items to rearrange",sum_of_chars) #solution part 1
 
+common_character = []
+start = 0
+for index in range(int(len(content)/3)):
+    current_group = content[start:start+3]
+    start+=3
+    first_elf = current_group[0].strip("\n")
+    second_elf = current_group[1].strip("\n")
+    third_elf = current_group[2].strip("\n")
+    common_character.append(string_intersection(first_elf,second_elf,third_elf))
+
+sum_of_chars = 0
+for char in common_character:
+    sum_of_chars += character_scores[char]
+print("Groups of three elves:",sum_of_chars) #solution part 2
